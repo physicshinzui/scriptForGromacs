ae6b019872aa2fafd3a13b4f1cc015df5b9e767a
shinji iida <physicshinzui@gmail.com>
2021-03-03 15:44:22 +0900

    An variable name was changed.

--- submit2TSUBAME.sh
+++ submit2TSUBAME.sh
@@ -6,7 +6,7 @@ readonly WALL_TIME='24:00:00'
 readonly JOB_NAME='MD simulation'
 readonly exe=$1
 
-nRuns=$(ls -1 npt_eq_*.log | wc -l)
-for (i=0; i<${nRuns}; i++); do
+nPreRuns=$(ls -1 npt_eq_*.log | wc -l)
+for (i=0; i<${nPreRuns}; i++); do
     qsub -g ${GROUP_NAME} -l q_node=1 -l h_rt=${WALL_TIME} -v id=${id} -N ${JOB_NAME} ${exe}
 done
