023faa108389103e5d9142e196774f69ff8c6f69
shinji iida <physicshinzui@gmail.com>
2021-03-03 15:43:37 +0900

    loop expression was changed.

--- submit2TSUBAME.sh
+++ submit2TSUBAME.sh
@@ -7,6 +7,6 @@ readonly JOB_NAME='MD simulation'
 readonly exe=$1
 
 nRuns=$(ls -1 npt_eq_*.log | wc -l)
-for id in $(seq 0 $(($nRuns - 1)) ); do
+for (i=0; i<${nRuns}; i++); do
     qsub -g ${GROUP_NAME} -l q_node=1 -l h_rt=${WALL_TIME} -v id=${id} -N ${JOB_NAME} ${exe}
 done
