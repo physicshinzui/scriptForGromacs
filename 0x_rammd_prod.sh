19a00e209872373c9da6a1481b8d31ecebfadb83
shinji iida <physicshinzui@gmail.com>
2021-03-07 12:18:51 +0900

    comment lines were removed.

--- 0x_rammd_prod.sh
+++ 0x_rammd_prod.sh
@@ -15,23 +15,6 @@ EOS
 id=$1
 make_mdp_file ramd prod $id
 
-# if [ -e npt_eq_${id}.tpr ] ; then
-#     cat templates/ramd_npt.mdp                   \
-#         | sed -e "s!#{RAND}!${RANDOM}!g"         \
-#         | sed -e "s!#{NSTEPS}!${nsteps}!g"       \
-#         | sed -e "s!#{TEMP}!${temp}!g"           \
-#         | sed -e "s!#{NSTXOUT}!${nstxout}!g"     \
-#         | sed -e "s!#{LOGOUT}!${nstlog}!g"       \
-#         | sed -e "s!#{SEED}!${RANDOM}!g"         \
-#         | sed -e "s!#{GROUP1}!${ramd_group1}!g"  \
-#         | sed -e "s!#{GROUP2}!${ramd_group2}!g"  \
-#         > ramd_prod${id}.mdp
-# else
-#     echo "There was no previous run ${id}."
-#     exit 
-
-# fi
-
 echo "NPT runs are running..."
 $GMX grompp -f ramd_prod_${id}.mdp  \
             -c npt_eq_${id}.gro    \
