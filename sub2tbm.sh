9513c32a909697ed64b76e2e210c446fff5233ac
shinji iida <physicshinzui@gmail.com>
2021-03-09 15:57:44 +0900

    priority option and reference URL were added.

--- sub2tbm.sh
+++ sub2tbm.sh
@@ -11,6 +11,8 @@ Usage:
             - restart.sh
         MD_TYPE: "npt or "ramd"
         nPreRuns: No. of the previous runs (>0)
+
+    See: https://helpdesk.t3.gsic.titech.ac.jp/manuals/handbook.ja/jobs/
 EOS
 
 readonly GROUP_NAME='hp170020'
@@ -19,8 +21,8 @@ readonly JOB_NAME='cMD'
 readonly exe=$1
 readonly MD_TYPE=$2
 readonly nPreRuns=$3
-
+readonly priority='-5' #Default
 for (( i=0; i<${nPreRuns}; i++ )); do
-    echo "qsub -g ${GROUP_NAME} -l q_node=1 -l h_rt=${WALL_TIME} -v id=${i} -N ${JOB_NAME} ${exe}"
-#    qsub -g ${GROUP_NAME} -l q_node=1 -l h_rt=${WALL_TIME} -v id=${id} -N ${JOB_NAME} ${exe}
+    echo "qsub -g ${GROUP_NAME} -p ${priority} -l q_node=1 -l h_rt=${WALL_TIME} -v id=${i} -N ${JOB_NAME} ${exe}"
+#    qsub -g ${GROUP_NAME} -p ${priority} -l q_node=1 -l h_rt=${WALL_TIME} -v id=${i} -N ${JOB_NAME} ${exe}
 done
