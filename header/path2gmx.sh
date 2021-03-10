a8fdb32371af6e5fe1c056539f4c14b84aa55bd7
Shinji Iida <siida@Shinjis-MacBook-Air.local>
2021-03-10 12:49:27 +0900

    path2gmx.sh and restart_setting.sh were modified a bit.

--- header/path2gmx.sh
+++ header/path2gmx.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 #GMX=/usr/local/bin/gmx
-readonly GMX=/Users/siida/Dropbox/01code/software/gromacs-ramd-release-2020/build/bin/gmx
+#readonly GMX=/Users/siida/Dropbox/01code/software/gromacs-ramd-release-2020/build/bin/gmx
+readonly GMX=/Users/siida//Dropbox/01code/software/gromacs-ramd-gromacs-2020.5-ramd-2.0-rc2/build/bin/gmx
--- header/restart_setting.sh
+++ header/restart_setting.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-readonly RST_N_STEPS=2010   # 
+readonly RST_N_STEPS=100   # 
 readonly T_EXTEND=0.1       # unit [ps]
 #readonly T_UNTIL=10        # unit [ps]
