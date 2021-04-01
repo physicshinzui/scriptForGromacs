e27391dcd08c3efb473d4414c2846945c2b1b336
Shinji Iida <physicshinzui@gmail.com>
2021-04-01 16:24:24 +0900

    inactivate lines in restart_setting.sh because they are not used at the moment.

--- header/restart_setting.sh
+++ header/restart_setting.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 readonly RST_N_STEPS=100   # 
-readonly T_EXTEND=0.1       # unit [ps]
+
+##Not used in this version
+#readonly T_EXTEND=0.1       # unit [ps]
 #readonly T_UNTIL=10        # unit [ps]
